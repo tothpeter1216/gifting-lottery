966f332656d588c8ad33ae224e8fc08e038cbbce
tothpeter1216 <tothpeter1216@gmail.com>
2020-11-27 18:37:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+app.use(express.json());
+
+app.post("/api/sendmail", async (request, response, next) => {
+  try {
+    const names = request.body.names;
+    //   const names = [
+    //     {
+    //       email: "tothpeter1216@gmail.com",
+    //       giftTo: "Mozart",
+    //     },
+    //     {
+    //       email: "7peregrin@gmail.com",
+    //       giftTo: "Beethoven",
+    //     },
+    //   ];
+    names.map((name) => {
+      sendMail(
+        name.email,
+        "Szia!\nEzt a levelet azért kaptad, mert valakinek a szervezésében részt vettél egy karácsonyi ajándékozós párosítás sorsolásában.\nValaki a te nevedet is 'kihúzta', de ami most fontosabb, neked is készülnöd kell egy ajándékkal. A következő személyt kell meglepned: " +
+          name.giftTo +
+          "     \n\nBoldog karácsonyt! :)"
+      );
+    });
+    response.json({ message: "it works" });
+  } catch (error) {
+    response.status(500).json(error);
+  }
+});
+
+const sendMail = (email, text) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+
+  var mailOptions = {
+    from: "codetheworlds@gmail.com",
+    to: email,
+    subject: "karácsony",
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+app.listen(process.env.PORT, () => {
+  console.log("The server is runing on port " + process.env.PORT);
+});
