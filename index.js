29eb9e31557f11fd758544410f4e63417f86812e
tothpeter1216 <tothpeter1216@gmail.com>
2021-01-21 12:45:29 +0100

    new build

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 
 const nodemailer = require("nodemailer");
-const { request, response } = require("express");
 require("dotenv").config();
 
 app.use(cors());
